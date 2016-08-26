db6d07d9e077ca517e78705a15ada1661a883d65
PrasannaPSL <sailakshmi.penta@netenrich.com>
2016-08-26 17:35:30 +0530

    Changed Hackthon Script according to New Azure Template

---
+++ Pending-Review/SaiLaxmi/Modify-AzureWebAppStatus.ps1
@@ -0,0 +1,425 @@
+﻿<#
+    .SYNOPSIS
+    Script to change the Azure Web App State.
+
+    .DESCRIPTION
+    Script to change the Azure Web App State.
+
+    .PARAMETER ClientID
+    ClientID of the client for whom the script is being executed.
+
+    .PARAMETER AzureUserName
+    User name for Azure login. This should be an Organizational account (not Hotmail/Outlook account)
+
+    .PARAMETER AzurePassword
+    Password for Azure user account.
+
+    .PARAMETER AzureSubscriptionID
+    Azure Subscription ID to use for this activity.
+
+    .PARAMETER ResourceGroupName
+    Name of the Azure ARM resource group to use for this command.
+
+    .PARAMETER AppName
+    Name of the Azure Web App.
+
+    .PARAMETER AppAction
+    Give one of the Web App states (Start/Restart/Stop).
+
+
+    .INPUTS
+     .\Modify-AzureWebAppStatus.ps1 -ClientID 1246 -AzureUserName sailakshmi.penta@netenrich.com -AzurePassword ***********
+    -AzureSubscriptionID ca68598c-ecc3-4abc-b7a2-1ecef33f278d -ResourceGroupName "testrg" -AppName testapp1235 
+    -AppAction Stop
+
+    .OUTPUTS
+     WARNING: The output object type of this cmdlet will be modified in a future release.
+    {
+        "Status":  "Success",
+        "BlobURI":  "https://nelogfiles.blob.core.windows.net/neportallogs/1246-Modify-AzureWebAppStatus-24-Aug-2016_152656.log",
+    
+        "Response":  [
+                         "Successfully 'Stopped' Azure Web App 'testapp1235'."
+                     ]
+    }
+
+    .NOTES
+     Purpose of script: Template for Azure Script to change the Azure Web App State
+     Minimum requirements: Azure PowerShell Version 1.4.0
+     Initially written by: Santhosh Gade
+     Update/revision History:
+     =======================
+     Updated by        Date            Reason
+     ==========        ====            ======
+     Santhosh Gade     14-05-2016      Hackthon
+     PSLPrasanna       24-08-2016      Change the script according to new Azure Template
+
+    .EXAMPLE
+
+    .\Modify-AzureWebAppStatus.ps1 -ClientID 1246 -AzureUserName sailakshmi.penta@netenrich.com -AzurePassword ***********
+     -AzureSubscriptionID ca68598c-ecc3-4abc-b7a2-1ecef33f278d -ResourceGroupName "testrg" -AppName testapp1235 
+     -AppAction Stop
+
+    WARNING: The output object type of this cmdlet will be modified in a future release.
+    {
+        "Status":  "Success",
+        "BlobURI":  "https://nelogfiles.blob.core.windows.net/neportallogs/1246-Modify-AzureWebAppStatus-24-Aug-2016_152656.log",
+    
+        "Response":  [
+                         "Successfully 'Stopped' Azure Web App 'testapp1235'."
+                     ]
+    }
+    
+    
+    .LINK
+    http://www.netenrich.com/#>
+
+[CmdletBinding()]
+Param
+(
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]$ClientID,
+
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]$AzureUserName,
+
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]$AzurePassword,
+
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]$AzureSubscriptionID,
+
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [String]$ResourceGroupName,
+    
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]$AppName,
+
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]$AppAction
+
+    # Add other parameters as required
+)
+
+Begin
+{
+    # Name the Log file based on script name
+    [DateTime]$LogFileTime = Get-Date
+    $FileTimeStamp = $LogFileTime.ToString("dd-MMM-yyyy_HHmmss")
+    $LogFileName = "$ClientID-$($MyInvocation.MyCommand.Name.Replace('.ps1',''))-$FileTimeStamp.log"
+    $LogFilePath = "C:\NEPortal\$LogFileName"
+
+    Function Get-BlobURIForLogFile
+    {
+        Try
+        {
+            $UC = Select-Xml -Path "C:\NEPortal\NEPortalApp.Config" -XPath configuration/appSettings -ErrorAction SilentlyContinue | Select -ExpandProperty Node | Select -ExpandProperty add
+            $UploadConfig = [ordered]@{}; $UC | % { $UploadConfig += @{ $_.key = $_.Value } }
+            $UploadConfig = [PSCustomObject]$UploadConfig
+
+            $Container = $UploadConfig.Container
+            $StorageAccName = $UploadConfig.StorageAccName
+            $StorageAccKey = $UploadConfig.StorageAccKey
+
+            ($context = New-AzureStorageContext -StorageAccountName $StorageAccName -StorageAccountKey $StorageAccKey -ErrorAction Stop) | Out-Null
+        }
+        Catch
+        {
+            Return "Error processing blob URI. Check if storage credentials are correct in 'C:\NEPortal\NEPortalApp.Config'"
+        }
+        Return "$($context.BlobEndPoint)$($UploadConfig.Container)/$($LogFilename)"
+    }
+
+    $LogFileBlobURI = Get-BlobURIForLogFile
+
+    # ======================================================================
+    # Write-Log function defination
+    # ======================================================================
+    Function Write-LogFile
+    {
+        Param([String]$FilePath, [String]$LogText, [Switch]$Overwrite = $false)
+
+        [DateTime]$LogTime = Get-Date
+        $TimeStamp = $LogTime.ToString("dd-MMM-yyyy hh:mm:ss tt")
+        $InputLine = "[$TimeStamp] : $LogText"
+
+        If($FilePath -like "*.???")
+        { $CheckPath = Split-Path $FilePath; }
+        Else
+        { $CheckPath = $FilePath }
+
+        If(Test-Path -Path $CheckPath -ErrorAction SilentlyContinue)
+        {
+            # Correct path Now check if it is a File or Folder
+            ($IsFolder = (Get-Item $FilePath -ErrorAction SilentlyContinue) -is [System.IO.DirectoryInfo]) | Out-Null
+            If($IsFolder)
+            {
+                If($FilePath.EndsWith("\")) { $FilePath = $FilePath.TrimEnd(1) }
+                $FilePath = "$FilePath\Log_$($LogTime.ToString('dd-MMM-yyyy_hh.mm.ss')).log"
+            }
+        }
+        Else
+        {
+            Try
+            {
+                If(-not($FilePath -like "*.???"))
+                {
+                    If($FilePath.EndsWith("\")) { $FilePath = $FilePath.TrimEnd(1) }
+                    $FilePath = "$FilePath\Log_$($LogTime.ToString('dd-MMM-yyyy_HH.mm.ss')).log"
+                    (New-Item -Path $FilePath -ItemType File -Force -ErrorAction Stop) | Out-Null
+                }
+                Else
+                {
+                    (New-Item -Path $CheckPath -ItemType Directory -Force -ErrorAction Stop) | Out-Null
+                }
+            }
+            Catch
+            { 
+                "Error creating output folder for Log file $(Split-Path $FilePath).`n$($Error[0].Exception.Message)"
+            }
+        }
+
+        If($Overwrite)
+        {
+            $InputLine | Out-File -FilePath $FilePath -Force
+        }
+        Else
+        {
+            $InputLine | Out-File -FilePath $FilePath -Force -Append
+        }
+    }
+
+    Write-LogFile -FilePath $LogFilePath -LogText "####[ Script Execution started: $($MyInvocation.MyCommand.Name). For Client ID: $ClientID ]####" -Overwrite
+
+    # Check minumum required version of Azure PowerShell
+    $AzurePSVersion = (Get-Module -ListAvailable -Name Azure -ErrorAction Stop).Version
+    If($AzurePSVersion -gt 1.4)
+    {
+        Write-LogFile -FilePath $LogFilePath -LogText "Required version of Azure PowerShell is available."
+    }
+    Else 
+    {
+        $ObjOut = "Required version of Azure PowerShell not available. Stopping execution.`nDownload and install required version from: http://aka.ms/webpi-azps."
+        $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+        Write-LogFile -FilePath $LogFilePath -LogText "$ObjOut`r`n<#BlobFileReadyForUpload#>"
+        Write-Output $output
+        Exit
+    }
+
+    Function Validate-AllParameters
+    {
+        Try
+        {
+            # Validate parameter: ClientID
+            Write-LogFile -FilePath $LogFilePath -LogText "Validating Parameters: ClientID. Only ERRORs will be logged."
+            If([String]::IsNullOrEmpty($ClientID))
+            {
+                Write-LogFile -FilePath $LogFilePath -LogText "Validation failed. ClientID parameter value is empty.`r`n<#BlobFileReadyForUpload#>"
+                $ObjOut = "Validation failed. ClientID parameter value is empty."
+                $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+                Write-Output $output
+                Exit
+            }
+
+            # Validate parameter: AzureUserName
+            Write-LogFile -FilePath $LogFilePath -LogText "Validating Parameters: AzureUserName. Only ERRORs will be logged."
+            If([String]::IsNullOrEmpty($AzureUserName))
+            {
+                Write-LogFile -FilePath $LogFilePath -LogText "Validation failed. AzureUserName parameter value is empty.`r`n<#BlobFileReadyForUpload#>"
+                $ObjOut = "Validation failed. AzureUserName parameter value is empty."
+                $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+                Write-Output $output
+                Exit
+            }
+
+            # Validate parameter: AzurePassword
+            Write-LogFile -FilePath $LogFilePath -LogText "Validating Parameters: AzurePassword. Only ERRORs will be logged."
+            If([String]::IsNullOrEmpty($AzurePassword))
+            {
+                Write-LogFile -FilePath $LogFilePath -LogText "Validation failed. AzurePassword parameter value is empty.`r`n<#BlobFileReadyForUpload#>"
+                $ObjOut = "Validation failed. AzurePassword parameter value is empty."
+                $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+                Write-Output $output
+                Exit
+            }
+
+            # Validate parameter: AzureSubscriptionID
+            Write-LogFile -FilePath $LogFilePath -LogText "Validating Parameters: AzureSubscriptionID. Only ERRORs will be logged."
+            If([String]::IsNullOrEmpty($AzureSubscriptionID))
+            {
+                Write-LogFile -FilePath $LogFilePath -LogText "Validation failed. AzureSubscriptionID parameter value is empty.`r`n<#BlobFileReadyForUpload#>"
+                $ObjOut = "Validation failed. AzureSubscriptionID parameter value is empty."
+                $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+                Write-Output $output
+                Exit
+            }           
+
+            # Validate parameter: ResourceGroupName
+            Write-LogFile -FilePath $LogFilePath -LogText "Validating Parameters: ResourceGroupName. Only ERRORs will be logged."
+            If([String]::IsNullOrEmpty($ResourceGroupName))
+            {
+                Write-LogFile -FilePath $LogFilePath -LogText "Validation failed. ResourceGroupName parameter value is empty.`r`n<#BlobFileReadyForUpload#>"
+                $ObjOut = "Validation failed. ResourceGroupName parameter value is empty."
+                $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+                Write-Output $output
+                Exit
+            }
+
+            # Validate parameter: AppName
+            Write-LogFile -FilePath $LogFilePath -LogText "Validating Parameters: AppName. Only ERRORs will be logged."
+            If([String]::IsNullOrEmpty($AppName))
+            {
+                Write-LogFile -FilePath $LogFilePath -LogText "Validation failed. AppName parameter value is empty.`r`n<#BlobFileReadyForUpload#>"
+                $ObjOut = "Validation failed. AppName parameter value is empty."
+                $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+                Write-Output $output
+                Exit
+            }
+
+            # Validate parameter: AppAction
+            Write-LogFile -FilePath $LogFilePath -LogText "Validating Parameters: AppAction. Only ERRORs will be logged."
+            If([String]::IsNullOrEmpty($AppAction))
+            {
+                Write-LogFile -FilePath $LogFilePath -LogText "Validation failed. AppAction parameter value is empty.`r`n<#BlobFileReadyForUpload#>"
+                $ObjOut = "Validation failed. AppAction parameter value is empty."
+                $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+                Write-Output $output
+                Exit
+            }
+
+            If($AppAction.Length -ne 0){
+                 Write-LogFile -FilePath $LogFilePath -LogText "Validating Parameters: AppAction. Only ERRORs will be logged."
+                 $arr = "Start","Stop","Restart"
+                 If($arr -notcontains $AppAction){
+                    Write-LogFile -FilePath $LogFilePath -LogText "Validation failed. PerformanceTear '$AppAction' is NOT a valid value for this parameter.`r`n<#BlobFileReadyForUpload#>"
+                    $ObjOut = "Validation failed. PerformanceTear '$AppAction' is not a valid value for this parameter."
+                    $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+                    Write-Output $output
+                    Exit
+                 }
+                 
+            }
+          
+        }
+        Catch
+        {
+            Write-LogFile -FilePath $LogFilePath -LogText "Error while validating parameters: $($Error[0].Exception.Message)`r`n<#BlobFileReadyForUpload#>"
+            $ObjOut = "Error while validating parameters: $($Error[0].Exception.Message)"
+            $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+            Write-Output $output
+            Exit
+        }
+    }
+
+    Function Login-ToAzureAccount
+    {
+        Try
+        {
+            Write-LogFile -FilePath $LogFilePath -LogText "Attempting to login to Azure RM subscription" 
+            $SecurePassword = ConvertTo-SecureString -AsPlainText $AzurePassword -Force
+            $Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $AzureUserName, $securePassword
+            (Login-AzureRmAccount -Credential $Cred -SubscriptionId $AzureSubscriptionID -ErrorAction Stop) | Out-Null
+            Write-LogFile -FilePath $LogFilePath -LogText "Login to Azure RM successful"
+        }
+        Catch
+        {
+            $ObjOut = "Error logging in to Azure Account.`n$($Error[0].Exception.Message)`r`n<#BlobFileReadyForUpload#>"
+            Write-LogFile -FilePath $LogFilePath -LogText $ObjOut
+            $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+            Write-Output $output
+            Exit
+        }
+    }
+}
+
+Process
+{
+    Validate-AllParameters
+
+    # 1. Login to Azure subscription
+    Login-ToAzureAccount
+
+    # 2. Check if Resource Group exists. Create Resource Group if it does not exist.
+    Try
+    {
+       Write-LogFile -FilePath $LogFilePath -LogText "Checking existance of resource group '$ResourceGroupName'"
+       $ResourceGroup = Get-AzureRmResourceGroup -Name $ResourceGroupName -ErrorAction Stop
+       Write-LogFile -FilePath $LogFilePath -LogText "Resource Group does exists"
+    }
+    Catch
+    {
+        $ObjOut = "Error while getting Azure Resource Group details.`r`n$($Error[0].Exception.Message)`r`n<#BlobFileReadyForUpload#>"
+        $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+        Write-Output $output
+        Write-LogFile -FilePath $LogFilePath -LogText $ObjOut
+        Exit
+    }
+
+
+    # 3. Check if WebApp exists.
+    Try
+    {
+        Write-LogFile -FilePath $LogFilePath -LogText "Checking existance of Web App '$AppName'"
+        $WACheck = Get-AzureRmWebApp -ResourceGroupName $ResourceGroupName -Name $AppName -ea Stop
+        Write-LogFile -FilePath $LogFilePath -LogText "WebApp '$AppName' already exists"       
+    }
+    Catch
+    {
+        $ObjOut = "Error while getting Azure WebApp details.`r`n$($Error[0].Exception.Message)`r`n<#BlobFileReadyForUpload#>"
+        $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+        Write-Output $output
+        Write-LogFile -FilePath $LogFilePath -LogText $ObjOut
+        Exit
+    }
+
+    #4. Change the Web App Status
+    Try
+    {
+        $status = (Get-AzureRmWebApp -ResourceGroupName $ResourceGroupName -Name $AppName).State        
+        If(($status -eq "Running" -and $AppAction -eq "Start" -or $AppAction -eq "Restart") -or ($status -eq "Stopped" -and $AppAction -eq "Stop")){
+           $ObjOut = "'$AppName' is already in '$status' status"
+           $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+           Write-Output $output
+           Write-LogFile -FilePath $LogFilePath -LogText $ObjOut
+           Exit
+        }
+        Else
+        {
+            for($i=0;$i -lt 3;$i++)
+            {
+                Write-LogFile -FilePath $LogFilePath -LogText "Trying to change the '$AppName' State."
+                If($AppAction -eq "Start"){
+                   $mstate = Start-AzureRmWebApp -Name $AppName -ResourceGroupName $ResourceGroupName -ErrorAction Stop
+                }
+                Elseif($AppAction -eq "Stop"){
+                    $mstate = Stop-AzureRmWebApp -Name $AppName -ResourceGroupName $ResourceGroupName -ErrorAction Stop
+                }
+                Elseif($AppAction -eq "Restart"){
+                    $mstate = Restart-AzureRmWebApp -Name $AppName -ResourceGroupName $ResourceGroupName -ErrorAction Stop
+                }
+
+                $mstatus = (Get-AzureRmWebApp -Name $AppName -ResourceGroupName $ResourceGroupName).State
+                If(($mstatus -eq "Running" -and $AppAction -eq "Start" -or $AppAction -eq "Restart") -or ($mstatus -eq "Stopped" -and $AppAction -eq "Stop")){
+                    $ObjOut = "Successfully '$mstatus' Azure Web App '$AppName'."
+                    $output = (@{"Response" = [Array]$ObjOut; Status = "Success"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+                    Write-Output $output 
+                    Write-LogFile -FilePath $LogFilePath -LogText $ObjOut    
+                    break              
+                }
+            }
+        }        
+    }
+    Catch
+    {
+        $ObjOut = "Error while Modifying Azure WebApp State.`r`n$($Error[0].Exception.Message)`r`n<#BlobFileReadyForUpload#>"
+        $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+        Write-Output $output
+        Write-LogFile -FilePath $LogFilePath -LogText $ObjOut
+        Exit
+    }
+}
+
+End
+{
+    Write-LogFile -FilePath $LogFilePath -LogText "####[ Script execution completed cuccessfully: $($MyInvocation.MyCommand.Name) ]####`r`n<#BlobFileReadyForUpload#>"
+}
