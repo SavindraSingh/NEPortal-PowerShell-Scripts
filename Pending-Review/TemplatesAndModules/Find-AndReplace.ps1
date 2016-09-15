0a825a4ff36a9db6bf4ecd5caca0ca26dbded937
savindrasingh.shahoo <savindrasingh.shahoo@netenrich.com>
2016-09-15 18:14:19 +0530

    Added find and replace script

---
+++ Pending-Review/TemplatesAndModules/Find-AndReplace.ps1
@@ -0,0 +1,33 @@
+﻿[CmdletBinding(PositionalBinding=$false)]
+Param
+(
+    [Parameter(Mandatory)]
+    [ValidateScript({ Test-Path -Path $_ -PathType Container})]
+    [String]$ScriptDirectoryName,
+
+    [Parameter(Mandatory)]
+    [String]$Find,
+
+    [Parameter(Mandatory)]
+    [String]$ReplaceWith,
+
+    [switch]$CaseSensitive = $false
+)
+
+Begin
+{
+    $Files = [System.IO.Directory]::GetFiles("C:\temp","*.ps1")
+}
+
+Process
+{
+    ForEach($File In $Files)
+    {
+        (Get-Content -Path $File -Force).Replace($Find, $ReplaceWith) | Set-Content -Path $File -Force
+    }
+}
+
+End
+{
+
+}
