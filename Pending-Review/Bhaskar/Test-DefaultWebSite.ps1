ff99b339cbe66392f29a2a752a0dbaaec0736fce
Bhaskar Desharaju <bhaskar.desharaju@netenrich.com>
2016-09-21 19:34:24 +0530

    To test the website availability

---
+++ Pending-Review/Bhaskar/Test-DefaultWebSite.ps1
@@ -0,0 +1,267 @@
+<#
+    .SYNOPSIS
+    Script to create <ResourceName/Type> in Azure Resource Manager Portal
+
+    .DESCRIPTION
+    Script to create <ResourceName/Type> in Azure Resource Manager Portal
+
+    .PARAMETER ClientID
+    ClientID of the client for whom the script is being executed.
+
+    .PARAMETER DefaultWebSiteUrl
+    User name for Azure login. This should be an Organizational account (not Hotmail/Outlook account)
+
+    .INPUTS
+    All parameter values in String format.
+
+    .OUTPUTS
+    String. Result of the command output.
+
+    .NOTES
+     Purpose of script: Script is to test the default website access
+     Minimum requirements: Azure PowerShell Version 1.4.0
+     Initially written by: Bhaskar Desharaju
+     Update/revision History:
+     =======================
+     Updated by        Date            Reason
+     ==========        ====            ======
+     SavindraSingh     26-May-16       Changed Mandatory=$True to Mandatory=$False for all parameters.
+     SavindraSingh     21-Jul-16       1. Added Login function in Begin block, instead of commands in Process block.
+                                       2. Check minumum required version of Azure PowerShell
+     SavindraSingh     26-Jul-16       1. Added flag for indicating log file readyness for uploading to blob in the log text.
+                                       2. Added Function Get-BlobURIForLogFile to return the URI for Log file blob in output.
+                                       3. Added Common parameter $ClientID to indicate the Client details in the logfile.
+    SavindraSingh      9-Sep-2016      1. Added a variable at script level (line 89) - $ScriptUploadConfig = $null
+                                       2. $Script:ScriptUploadConfig will now hold the value for the current required version
+                                          of Azure PowerShell. Which is used at line 176 with - If($AzurePSVersion -gt $ScriptUploadConfig.RequiredPSVersion)
+                                          to check if we have Azure PowerShell version available.
+                                       3. The required version of Azure PowerShell should now be mentioned in the NEPortalApp.Config as given below:
+                                          Under <appSettings> tag - <add key="RequiredPSVersion" value="2.0.1"/>
+
+    .EXAMPLE
+    C:\PS> 
+
+    .EXAMPLE
+    C:\PS> 
+
+    .LINK
+    http://www.netenrich.com/#>
+
+[CmdletBinding()]
+Param
+(
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]$ClientID,
+
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]$DefaultWebSiteUrl
+)
+
+Begin
+{
+    $OldProgressPreference = $ProgressPreference
+    $ProgressPreference = 'SilentlyContinue'
+    # Name the Log file based on script name
+    [DateTime]$LogFileTime = Get-Date
+    $FileTimeStamp = $LogFileTime.ToString("dd-MMM-yyyy_HHmmss")
+    $LogFileName = "$ClientID-$($MyInvocation.MyCommand.Name.Replace('.ps1',''))-$FileTimeStamp.log"
+    $LogFilePath = "C:\NEPortal\$LogFileName"
+
+    $ScriptUploadConfig = $null
+    Function Get-BlobURIForLogFile
+    {
+        Try
+        {
+            $UC = Select-Xml -Path "C:\NEPortal\NEPortalApp.Config" -XPath configuration/appSettings -ErrorAction SilentlyContinue | Select -ExpandProperty Node | Select -ExpandProperty add
+            $UploadConfig = [ordered]@{}; $UC | % { $UploadConfig += @{ $_.key = $_.Value } }
+            $Script:ScriptUploadConfig = [PSCustomObject]$UploadConfig
+
+            $Container = $ScriptUploadConfig.Container
+            $StorageAccName = $ScriptUploadConfig.StorageAccName
+            $StorageAccKey = $ScriptUploadConfig.StorageAccKey
+
+            ($context = New-AzureStorageContext -StorageAccountName $StorageAccName -StorageAccountKey $StorageAccKey -ErrorAction Stop) | Out-Null
+        }
+        Catch
+        {
+            Return "Error processing blob URI. Check if storage credentials are correct in 'C:\NEPortal\NEPortalApp.Config'"
+        }
+        Return "$($context.BlobEndPoint)$($ScriptUploadConfig.Container)/$($LogFilename)"
+    }
+
+    $LogFileBlobURI = Get-BlobURIForLogFile
+
+    # ======================================================================
+    # Write-Log function defination
+    # ======================================================================
+    Function Write-LogFile
+    {
+        Param([String]$FilePath, [String]$LogText, [Switch]$Overwrite = $false)
+
+        [DateTime]$LogTime = Get-Date
+        $TimeStamp = $LogTime.ToString("dd-MMM-yyyy hh:mm:ss tt")
+        $InputLine = "[$TimeStamp] : $LogText"
+
+        If($FilePath -like "*.???")
+        { $CheckPath = Split-Path $FilePath; }
+        Else
+        { $CheckPath = $FilePath }
+
+        If(Test-Path -Path $CheckPath -ErrorAction SilentlyContinue)
+        {
+            # Correct path Now check if it is a File or Folder
+            ($IsFolder = (Get-Item $FilePath -ErrorAction SilentlyContinue) -is [System.IO.DirectoryInfo]) | Out-Null
+            If($IsFolder)
+            {
+                If($FilePath.EndsWith("\")) { $FilePath = $FilePath.TrimEnd(1) }
+                $FilePath = "$FilePath\Log_$($LogTime.ToString('dd-MMM-yyyy_hh.mm.ss')).log"
+            }
+        }
+        Else
+        {
+            Try
+            {
+                If(-not($FilePath -like "*.???"))
+                {
+                    If($FilePath.EndsWith("\")) { $FilePath = $FilePath.TrimEnd(1) }
+                    $FilePath = "$FilePath\Log_$($LogTime.ToString('dd-MMM-yyyy_HH.mm.ss')).log"
+                    (New-Item -Path $FilePath -ItemType File -Force -ErrorAction Stop) | Out-Null
+                }
+                Else
+                {
+                    (New-Item -Path $CheckPath -ItemType Directory -Force -ErrorAction Stop) | Out-Null
+                }
+            }
+            Catch
+            { 
+                "Error creating output folder for Log file $(Split-Path $FilePath).`n$($Error[0].Exception.Message)"
+            }
+        }
+
+        If($Overwrite)
+        {
+            $InputLine | Out-File -FilePath $FilePath -Force
+        }
+        Else
+        {
+            $InputLine | Out-File -FilePath $FilePath -Force -Append
+        }
+    }
+
+    Write-LogFile -FilePath $LogFilePath -LogText "####[ Script Execution started: $($MyInvocation.MyCommand.Name). For Client ID: $ClientID ]####" -Overwrite
+
+    # Check minumum required version of Azure PowerShell
+    $AzurePSVersion = (Get-Module -ListAvailable -Name Azure -ErrorAction Stop).Version
+    If($AzurePSVersion -ge $ScriptUploadConfig.RequiredPSVersion)
+    {
+        Write-LogFile -FilePath $LogFilePath -LogText "Required version of Azure PowerShell is $($ScriptUploadConfig.RequiredPSVersion). Current version on host machine is $($AzurePSVersion.ToString())."
+    }
+    Else 
+    {
+        $ObjOut = "Required version of Azure PowerShell not available. Stopping execution.`nDownload and install required version from: http://aka.ms/webpi-azps.`
+        `r`nRequired version of Azure PowerShell is $($ScriptUploadConfig.RequiredPSVersion). Current version on host machine is $($AzurePSVersion.ToString())."
+        $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+        Write-LogFile -FilePath $LogFilePath -LogText "$ObjOut`r`n<#BlobFileReadyForUpload#>"
+        Write-Output $output
+        Exit
+    }
+
+    Function Validate-AllParameters
+    {
+        Try
+        {
+            # Validate parameter: ClientID
+            Write-LogFile -FilePath $LogFilePath -LogText "Validating Parameters: ClientID. Only ERRORs will be logged."
+            If([String]::IsNullOrEmpty($ClientID))
+            {
+                Write-LogFile -FilePath $LogFilePath -LogText "Validation failed. ClientID parameter value is empty.`r`n<#BlobFileReadyForUpload#>"
+                $ObjOut = "Validation failed. ClientID parameter value is empty."
+                $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+                Write-Output $output
+                Exit
+            }
+
+            # Validate parameter: DefaultWebSiteUrl
+            Write-LogFile -FilePath $LogFilePath -LogText "Validating Parameters: DefaultWebSiteUrl. Only ERRORs will be logged."
+            If([String]::IsNullOrEmpty($DefaultWebSiteUrl))
+            {
+                Write-LogFile -FilePath $LogFilePath -LogText "Validation failed. DefaultWebSiteUrl parameter value is empty.`r`n<#BlobFileReadyForUpload#>"
+                $ObjOut = "Validation failed. DefaultWebSiteUrl parameter value is empty."
+                $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+                Write-Output $output
+                Exit
+            }
+        }
+        Catch
+        {
+            Write-LogFile -FilePath $LogFilePath -LogText "Error while validating parameters: $($Error[0].Exception.Message)`r`n<#BlobFileReadyForUpload#>"
+            $ObjOut = "Error while validating parameters: $($Error[0].Exception.Message)"
+            $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+            Write-Output $output
+            Exit
+        }
+    }
+
+    Function Login-ToAzureAccount
+    {
+        Try
+        {
+            Write-LogFile -FilePath $LogFilePath -LogText "Attempting to login to Azure RM subscription" 
+            $SecurePassword = ConvertTo-SecureString -AsPlainText $AzurePassword -Force
+            $Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $AzureUserName, $securePassword
+            (Login-AzureRmAccount -Credential $Cred -SubscriptionId $AzureSubscriptionID -ErrorAction Stop) | Out-Null
+            Write-LogFile -FilePath $LogFilePath -LogText "Login to Azure RM successful"
+        }
+        Catch
+        {
+            $ObjOut = "Error logging in to Azure Account.`n$($Error[0].Exception.Message)`r`n<#BlobFileReadyForUpload#>"
+            Write-LogFile -FilePath $LogFilePath -LogText $ObjOut
+            $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+            Write-Output $output
+            Exit
+        }
+    }
+}
+
+Process
+{
+    # 1. Validate All parameters
+    Validate-AllParameters
+
+    # 2. Testing the Website access
+    Try 
+    {
+        ($StatusCheck = Invoke-WebRequest -Uri $DefaultWebSiteUrl -ErrorAction Stop -WarningAction SilentlyContinue) | Out-Null
+        if($StatusCheck.StatusCode -eq 200)
+        {
+            Write-LogFile -FilePath $LogFilePath -LogText "Website $DefaultWebSiteUrl test is successful and the Status is: $($StatusCheck.StatusDescription)"
+            $ObjOut = "Website $DefaultWebSiteUrl test is successful and the Status is: $($StatusCheck.StatusDescription)"
+            $output = (@{"Response" = [Array]$ObjOut; Status = "Success"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+            Write-Output $output            
+        }
+        Else 
+        {
+            Write-LogFile -FilePath $LogFilePath -LogText "Website $DefaultWebSiteUrl is not accessible and Status is: $($StatusCheck.StatusDescription)"
+            $ObjOut = "Website $DefaultWebSiteUrl is not accessible and Status is: $($StatusCheck.StatusDescription)"
+            $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+            Write-Output $output
+            exit
+        }
+    }
+    Catch 
+    {
+        $ObjOut = "Error while checking the access to website $DefaultWebSiteUrl.$($Error[0].Exception.Message)"
+        $output = (@{"Response" = [Array]$ObjOut; Status = "Failed"; BlobURI = $LogFileBlobURI} | ConvertTo-Json).ToString().Replace('\u0027',"'")
+        Write-Output $output
+        Write-LogFile -FilePath $LogFilePath -LogText "$ObjOut`r`n<#BlobFileReadyForUpload#>"
+        Exit
+    }
+    Finally
+    {
+        $ProgressPreference = $OldProgressPreference
+    }
+}
+End
+{
+    Write-LogFile -FilePath $LogFilePath -LogText "####[ Script execution completed Successfully: $($MyInvocation.MyCommand.Name) ]####`r`n<#BlobFileReadyForUpload#>"
+}
